f43a4a7936d3b836edfdb684fe77fe3506a76be1
Manul Perera <perera102at@u.agri.ruh.ac.lk>
2025-10-17 10:49:14 +0530

    Switch database connection to PDO

--- config.php
+++ config.php
@@ -1,4 +1,15 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'shop_db') 
-        or die('connection failed');
+
+$db_name = 'mysql:host=localhost;dbname=shop_db';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO($db_name, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die('connection failed: ' . $e->getMessage());
+}
+
 ?>
